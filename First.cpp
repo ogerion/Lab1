30fc875a25d83024f1483cb1c25bc35825c5412c
Владимир Бредихин <fragebum@gmail.com>
2024-11-09 12:34:02 +0300

    First.cpp with main func & CMakeLists

---
+++ First.cpp
@@ -0,0 +1,49 @@
+﻿#include "tests/UnqPtrTest.h"
+#include "tests/ShrdPtrTest.h"
+#include "tests/WeakPtrTest.h"
+#include "tests/MsPtrTest.h"
+#include "tests/MemorySpanTest.h"
+#include <matplot/matplot.h>
+#include "tests/PerfomanceTest.h"
+
+using namespace std;
+using namespace matplot;
+
+
+
+int main()
+{
+	testUnqPtrBasic();
+	testUnqPtrMove();
+	testUnqPtrRelease();
+
+	testShrdPtrBasic();
+	testShrdPtrMove();
+	testShrdPtrReset();
+	testShrdPtrCounter();
+	testShrdPtrDestroy();
+	testShrdPtrTypeCast();
+	testShrdPtrCompatibilityWeak();
+
+	testWeakPtrBasic();
+	testWeakPtrMove();
+	testWeakPtrReset();
+	testWeakPtrCounter();
+	testWeakPtrDestroy();
+	testWeakPtrTypeCast();
+
+	testMsPtrBasic();
+	testMsPtrCopy();
+	testMsPtrGet();
+	testMsPtrSet();
+
+	testMemorySpanBasic();
+	testMemorySpanConcat();
+	testMemorySpanGet();
+	testMemorySpanCopy();
+
+	vizualize();
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	_CrtDumpMemoryLeaks();
+	return 0;
+}
